256db7f0a490abb8875ffce0c1de01630b28d9ac
jennifertigner <jennifertigner@gmail.com>
2015-10-15 14:27:14 -0700

    Update array_string_length.rb
    Fixing whitespace

--- Ruby/array_string_length.rb
+++ Ruby/array_string_length.rb
@@ -1,9 +1,8 @@
 #RubyMonk Assignment: Given an array containing some strings, return an array containing the length of those strings.
 
-
 def length_finder(input_array)
-	conversion = lambda {|string| string.length}	# Block to calculate string length
-	input_array.collect!(&conversion)				# Map each element using block
+	conversion = lambda {|string| string.length}			# Block to calculate string length
+	input_array.collect!(&conversion)									# Map each element using block
 	print input_array
 end
 
